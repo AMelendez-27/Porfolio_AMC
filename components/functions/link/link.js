3f354adaf393fae42da2726bf58abe64c31ecf6c
Àlex Meléndez <alex27mel@gmail.com>
2025-01-15 15:44:03 +0100

    .
    .

--- components/functions/link/link.js
+++ components/functions/link/link.js
@@ -6,8 +6,20 @@ export const addLink = (linkNme) => {
     document.addEventListener('DOMContentLoaded', () => {
       const projectsContainer = document.getElementsByClassName('footer-links-container')[0];
 
-      const linkButton = document.createElement('button')
+      const linkButton = document.createElement('a')
       linkButton.className = `footer link-button`
+      
+      function link (linkNme) {
+        const linkedin = "linkedin2";
+        const github = "github2";
+        if (linkNme === linkedin) {
+          linkButton.href = 'https://www.linkedin.com/in/%C3%A0lex-mel%C3%A9ndez-centeno-a76353337/'
+        }
+
+        if ( linkNme === github) {
+          linkButton.href = 'https://github.com/AMelendez-27'
+        }
+      }
 
       const linkIcon = document.createElement('img')
       linkIcon.className = `footer link-icon`
